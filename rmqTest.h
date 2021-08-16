f68e12af77813f2411557b154dfe9bb1823da33e
Arne (Wahrstorf) <aleitert@kent.edu>
2021-08-16 21:05:00 +0200

    Added empty files to test RMQ algorithms.

---
+++ rmqTest.h
@@ -0,0 +1,12 @@
+// Defines functions to compare the correctnes and runtime of RMQ algorithms.
+
+#ifndef __RmqTest_HPP__
+#define __RmqTest_HPP__
+
+#include "rmq.hpp"
+
+namespace RMQTest
+{
+}
+
+#endif
