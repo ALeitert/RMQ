7f9e6a493f099767ef14558a9c29b49c91d47055
Arne (Wahrstorf) <aleitert@kent.edu>
2021-08-17 01:13:47 +0200

    Added code to create segment tree.

--- segTreeRmq.hpp
+++ segTreeRmq.hpp
@@ -20,6 +20,80 @@ public:
     // Pre-processes the data to allow queries.
     void processData()
     {
+        // Shortcut to access data.
+        const std::vector<T>& data = this->data;
+        const size_t n = data.size();
+
+
+        // --- Build tree. ---
+
+        // Determine size.
+        size_t treeSize = n;
+        for
+        (
+            size_t laySz = n;
+            laySz > 1;
+            laySz = (laySz + 1) >> 1, treeSize += laySz
+        ) { }
+
+        tree.resize(treeSize);
+
+        // Build bottom layer.
+        for (size_t i = 0; i < n; i++)
+        {
+            Node& node = tree[treeSize - n + i];
+            node.minIdx = i;
+            node.frIdx = i;
+            node.toIdx = i;
+        }
+
+        // Build tree bottom-up.
+        for
+        (
+            size_t qSize = n, qStart = treeSize - n;
+            qSize > 1;
+            qSize = (qSize + 1) >> 1, qStart -= qSize
+        )
+        {
+            // Outer loop has one iteration per layer.
+
+            //  qSize: The number of nodes in the previous layer.
+            // qStart: The index of the first (i.e. left-most) node of the
+            //         previous layer in tree[].
+
+            for
+            (
+                size_t nPtr = qStart - ((qSize + 1) >> 1), qPtr = qStart;
+                qPtr < qSize + qStart;
+                nPtr++, qPtr += 2
+            )
+            {
+                // The inner loop iterates over the nodes in the layer below and
+                // "creates" the nodes of the current layer.
+
+                // nPtr: The index in tree[] of the "new" node.
+                // qPtr: The index in tree[] of the new node's first child.
+
+                Node& node = tree[nPtr];
+
+                node.left = &tree[qPtr];
+                node.frIdx = node.left->frIdx;
+                node.toIdx = node.left->toIdx;
+                node.minIdx = node.left->minIdx;
+
+                // Still one more element?
+                if (qPtr + 1 < qSize + qStart)
+                {
+                    node.right = &tree[qPtr + 1];
+                    node.toIdx = node.right->toIdx;
+
+                    if (data[node.right->minIdx] < data[node.minIdx])
+                    {
+                        node.minIdx = node.right->minIdx;
+                    }
+                }
+            }
+        }
     }
 
     // Performs a query on the given data and given range.
@@ -29,6 +103,25 @@ public:
     size_t operator()(size_t i, size_t j) const
     {
     }
+
+
+private:
+
+    struct Node
+    {
+        Node* left  = nullptr;
+        Node* right = nullptr;
+
+        // The index range represented by the node.
+        size_t frIdx = -1;
+        size_t toIdx = -1;
+
+        size_t minIdx = -1;
+    };
+
+    // Stores the nodes of the segment tree.
+    std::vector<Node> tree;
+
 };
 
 #endif
