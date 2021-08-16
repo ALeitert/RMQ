d9a631aab20b79883756d0a782e7e54e46d3a89e
Arne (Wahrstorf) <aleitert@kent.edu>
2021-08-17 00:02:41 +0200

    Added helper function to print time.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,62 @@
 //     LATIN 2000, LNCS 1776, 88-94, 2000.
 
 
+#include <iomanip>
 #include <iostream>
 
 
 using namespace std;
 
+
+// Prints the given time (in milliseconds) into the given stream using
+// appropriate units.
+void printTime(int64_t time, ostream& out)
+{
+    constexpr int64_t milSec = 1;
+    constexpr int64_t decSec = 100 * milSec;
+    constexpr int64_t second = 10 * decSec;
+    constexpr int64_t minute = 60 * second;
+    constexpr int64_t hour = 60 * minute;
+    constexpr int64_t day = 24 * hour;
+
+    if (time < second)
+    {
+        out << setw(3) << time << " ms";
+    }
+    else if (time <= minute)
+    {
+        int64_t dSecs = (time + decSec - 1) / decSec;
+        int64_t secs = dSecs / 10;
+        dSecs = dSecs % 10;
+
+        out << setw(2) << secs << "." << dSecs << " s";
+    }
+    else if (time <= hour)
+    {
+        int64_t secs = (time + second - 1) / second;
+        int64_t mins = secs / 60;
+        secs = secs % 60;
+
+        out << setw(2) << mins << " min " << setw(2) << secs << " s";
+    }
+    else if (time <= day)
+    {
+        int64_t mins = (time + minute - 1) / minute;
+        int64_t hours = mins / 60;
+        mins = mins % 60;
+
+        out << setw(2) << hours << " h " << setw(2) << " min";
+    }
+    else // time > day
+    {
+        int64_t hours = (time + hour - 1) / hour;
+        int64_t days = hours / 24;
+        hours = hours % 24;
+
+        out << days << " d " << setw(2) << hour << " h";
+    }
+    out << flush;
+}
 
 int main()
 {
