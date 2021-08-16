ce17c32ebe87ffdbe5cee08fa74301b0bbeb11b0
Arne (Wahrstorf) <aleitert@kent.edu>
2021-08-16 17:18:21 +0200

    Initial commit with 'empty' main.cpp and .gitignore file.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Implementation of the Range Minimum Query algorithms presented in [1].
+
+// [1] M.A. Bender, M. Farach-Colton:
+//     The LCA Problem Revisited.
+//     LATIN 2000, LNCS 1776, 88-94, 2000.
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+
+}
