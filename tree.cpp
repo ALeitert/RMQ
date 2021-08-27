89e517b0fcfc6617e64fd0ab9330564c656f8180
Arne (Wahrstorf) <aleitert@kent.edu>
2021-08-27 08:15:57 +0200

    Added basic implementation for Tree class.

--- tree.cpp
+++ tree.cpp
@@ -1,1 +1,59 @@
 #include "tree.h"
+
+
+using namespace std;
+
+
+// Default constructor.
+// Creates an empty tree.
+Tree::Tree() { /* Nothing. */ }
+
+// Constructor.
+Tree::Tree(const vector<size_t>& parList) :
+    parents(parList)
+{
+    buildChildren();
+}
+
+// Constructor.
+Tree::Tree(vector<size_t>&& parList) :
+    parents(move(parList))
+{
+    buildChildren();
+}
+
+
+// Returns the parent's ID of the given node.
+size_t Tree::operator()(size_t uId) const
+{
+    return parents[uId];
+}
+
+// Returns the children of the given node.
+const vector<size_t>& Tree::operator[](size_t uId) const
+{
+    return children[uId];
+}
+
+
+
+// Helper function for constructor.
+void Tree::buildChildren()
+{
+    children.resize(parents.size());
+
+    for (size_t uId = 0; uId < parents.size(); uId++)
+    {
+        size_t pId = parents[uId];
+
+        if (pId == NullNode)
+        {
+            root = uId;
+        }
+        else
+        {
+            children[pId].push_back(uId);
+        }
+    }
+}
+
--- tree.h
+++ tree.h
@@ -1,11 +1,55 @@
-// Represents a tree.
+// Represents a rooted tree.
 
 #ifndef __Tree_H__
 #define __Tree_H__
 
 
+#include <vector>
+
+
 class Tree
 {
+    // Shortcut to avoid the need for "std::".
+    template<typename X> using vector = std::vector<X>;
+
+    // Represents a null pointer.
+    const size_t NullNode = -1;
+
+
+public:
+
+    // Default constructor.
+    // Creates an empty tree.
+    Tree();
+
+    // Constructor.
+    Tree(const vector<size_t>& parList);
+
+    // Constructor.
+    Tree(vector<size_t>&& parList);
+
+
+    // Returns the parent's ID of the given node.
+    size_t operator()(size_t uId) const;
+
+    // Returns the children of the given node.
+    const vector<size_t>& operator[](size_t uId) const;
+
+
+private:
+
+    // The ID of the root node.
+    size_t root = NullNode;
+
+    // The parents of each node.
+    vector<size_t> parents;
+
+    // The children of each node.
+    vector<vector<size_t>> children;
+
+
+    // Helper function for constructors.
+    void buildChildren();
 
 };
 
