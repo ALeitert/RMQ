2410f6173e2e9d434ecd4e3f71fd0008fed93119
Arne (T440s) <aleitert@kent.edu>
2021-11-30 23:26:11 +0800

    Improved computation of logarithm.

--- log.hpp
+++ log.hpp
@@ -3,41 +3,36 @@
 #ifndef __Log_H__
 #define __Log_H__
 
-// Returns the ceil of the logarithm base 2 of the given number.
-inline unsigned logC(unsigned n)
-{
-    return (sizeof(unsigned) << 3) - __builtin_clz((n - 1) | 1);
-}
-
-// Returns the ceil of the logarithm base 2 of the given number.
-inline unsigned logC(unsigned long n)
-{
-    return (sizeof(unsigned long) << 3) - __builtin_clzl((n - 1) | 1);
-}
-
-// Returns the ceil of the logarithm base 2 of the given number.
-inline unsigned logC(unsigned long long n)
-{
-    return (sizeof(unsigned long long) << 3) - __builtin_clzll((n - 1) | 1);
-}
-
+#ifdef __GNUC__
 
 // Returns the floor of the logarithm base 2 of the given number.
 inline unsigned logF(unsigned n)
 {
-    return (sizeof(unsigned) << 3) - __builtin_clz(n | 1) - 1;
+    return (sizeof(unsigned) * __CHAR_BIT__ - 1) xor __builtin_clz(n | 1);
 }
 
 // Returns the floor of the logarithm base 2 of the given number.
 inline unsigned logF(unsigned long n)
 {
-    return (sizeof(unsigned long) << 3) - __builtin_clzl(n | 1) - 1;
+    return (sizeof(unsigned long) * __CHAR_BIT__ - 1) xor __builtin_clzl(n | 1);
 }
 
 // Returns the floor of the logarithm base 2 of the given number.
 inline unsigned logF(unsigned long long n)
 {
-    return (sizeof(unsigned long long) << 3) - __builtin_clzll(n | 1) - 1;
+    return (sizeof(unsigned long long) * __CHAR_BIT__ - 1) xor __builtin_clzll(n | 1);
+}
+
+#else
+
+#endif
+
+
+// Returns the ceil of the logarithm base 2 of the given number.
+template<class T>
+inline unsigned logC(T n)
+{
+    return logF(n - 1) + 1;
 }
 
 #endif
